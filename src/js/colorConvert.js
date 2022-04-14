8f6193478f2891808f2eb0eb5611e18c6bfcfcc2
dkedid <73521518+dkedid@users.noreply.github.com>
2022-04-15 00:12:21 +0900

    d

--- src/js/colorConvert.js
+++ src/js/colorConvert.js
@@ -3,6 +3,8 @@ const colorSelector = document.querySelector(".input-color");
 const downLoad = document.querySelector(".download-anchor");
 const colorInf = document.querySelector(".config-color-title");
 
+let isLoaded = false;
+
 /* 색상 변경 */
 const handleColor = () => {
     const colorValue = colorSelector.value;
@@ -14,18 +16,82 @@ colorSelector.addEventListener("input", handleColor);
 
 /* 다운로드 */
 const handleDown = async () => {
-    const convertedDiv = await html2canvas(canvasDiv);
-    const url = convertedDiv.toDataURL("image/jpeg");
-    console.log(url);
+    let imgURL = "https://i.imgur.com/qEhGoU1.png";
+    downloadedImg = new Image();
+    downloadedImg.crossOrigin = "Anonymous";
+    downloadedImg.addEventListener("load", imageReceived, false);
+    downloadedImg.src = imgURL;
 };
+
+const imageReceived = async () => {
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    console.log(downloadedImg);
+
+    downloadedImg.width = 500;
+    downloadedImg.height = 500;
+
+    canvas.width = downloadedImg.width;
+    canvas.height = downloadedImg.height;
+
+    context.drawImage(downloadedImg, 0, 0);
+    if (!isLoaded) {
+        isLoaded = true;
+        canvasDiv.appendChild(canvas);
+    }
+    try {
+        const convertedDiv = await new html2canvas(canvasDiv, {
+            backgroundColor: "transparent",
+            allowTaint: true,
+            useCORS: false,
+        });
+        const url = convertedDiv.toDataURL("image/jpeg");
+        console.log(url);
+        downloadURI(url, "Pengoose.jpeg");
+    } catch (err) {
+        console.log("Error: " + err);
+    }
+};
+
+/*
+const convertedDiv = await new html2canvas(canvasDiv, {
+    backgroundColor: "transparent",
+    allowTaint: true,
+    useCORS: false,
+});
+const url = convertedDiv.toDataURL("image/jpeg");
+console.log(url);
+downloadURI(url, "Pengoose.jpeg");
+};*/
 
 const downloadURI = (uri, name) => {
     var link = document.createElement("a");
-
     link.download = name;
     link.href = uri;
     document.body.appendChild(link);
     link.click();
 };
+
+/*
+//downLoad
+const handleDown = async () => {
+    const convertedDiv = await new html2canvas(canvasDiv, {
+        backgroundColor: "transparent",
+        allowTaint: true,
+        useCORS: true,
+    });
+    const url = convertedDiv.toDataURL("image/jpeg");
+    console.log(url);
+    downloadURI(url, "Pengoose.jpeg");
+};
+
+const downloadURI = (uri, name) => {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+};
+*/
 
 downLoad.addEventListener("click", handleDown);
