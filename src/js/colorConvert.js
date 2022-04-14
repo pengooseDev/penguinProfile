323265c3c5095e89acaefaed111eb05369a45f13
dkedid <73521518+dkedid@users.noreply.github.com>
2022-04-14 21:53:52 +0900

    Update colorConvert.js

--- src/js/colorConvert.js
+++ src/js/colorConvert.js
@@ -21,6 +21,7 @@ const handleDown = async () => {
 
 const downloadURI = (uri, name) => {
     var link = document.createElement("a");
+
     link.download = name;
     link.href = uri;
     document.body.appendChild(link);
